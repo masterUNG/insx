5e2703d76efacbde60661e9b47315a0d2703ef40
singhto nonklang <singhto1983@gmail.com>
2021-02-18 16:55:45 +0700

    upload after master 18feb

--- lib/pages/insx_page.dart
+++ lib/pages/insx_page.dart
@@ -1,8 +1,13 @@
 import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+import 'dart:ui';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:psinsx/models/insx_model.dart';
+import 'package:psinsx/models/insx_model2.dart';
 import 'package:psinsx/pages/map_insx.dart';
 import 'package:psinsx/utility/my_style.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -18,6 +23,9 @@ class _InsxPageState extends State<InsxPage> {
   bool loadStatus = true; //โหลด
   bool status = true; //มีข้อมูล
   List<InsxModel> insxModels = List();
+  List<InsxModel2> insxModel2s = [];
+  List<Color> colorIcons = List();
+  List<File> files = List();
 
   @override
   void initState() {
@@ -28,7 +36,6 @@ class _InsxPageState extends State<InsxPage> {
   Future<Null> readInsx() async {
     SharedPreferences preferences = await SharedPreferences.getInstance();
     String workername = preferences.getString('staffname');
-    //print('workername === $workername');
 
     String url =
         'https://pea23.com/apipsinsx/getInsxWhereUser.php?isAdd=true&worker_name=$workername';
@@ -38,17 +45,13 @@ class _InsxPageState extends State<InsxPage> {
       });
 
       if (value.toString() != 'null') {
-        //print('value ====>>> $value');
-
         var result = json.decode(value.data);
-
-        print('result ===>>> $result');
-
         for (var map in result) {
-          InsxModel insxModel = InsxModel.fromJson(map);
+          InsxModel2 insxModel2 = InsxModel2.fromMap(map);
           setState(() {
-            insxModels.add(insxModel);
-            //print('map $map');
+            insxModel2s.add(insxModel2);
+            colorIcons.add(calculageHues(insxModel2.noti_date));
+            files.add(null);
           });
         }
       } else {
@@ -62,24 +65,27 @@ class _InsxPageState extends State<InsxPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
         body: ListView(
-          children: <Widget>[
-            Card(
-              child: ListTile(
-                title: Text('จำนวน : 123 รายการ'),
-                trailing: IconButton(icon: Icon(Icons.map), onPressed: (){
-                  MaterialPageRoute materialPageRoute = MaterialPageRoute(
-                      builder: (context) => MapInsx(),
+      children: <Widget>[
+        Card(
+          child: ListTile(
+              title: Text(insxModel2s.length == 0
+                  ? 'จำนวน : ? รายการ'
+                  : 'จำนวน : ${insxModel2s.length} รายการ'),
+              trailing: IconButton(
+                  icon: Icon(Icons.map),
+                  onPressed: () {
+                    MaterialPageRoute materialPageRoute = MaterialPageRoute(
+                      builder: (context) => MapInsx(
+                        insxModel2s: insxModel2s,
+                      ),
                     );
                     Navigator.push(context, materialPageRoute);
-                })
-               
-              ),
-            ),
-            loadStatus ? MyStyle().showProgress() : showContent(),
-          ],
-        ));
+                  })),
+        ),
+        loadStatus ? MyStyle().showProgress() : showContent(),
+      ],
+    ));
   }
 
   Widget showContent() {
@@ -90,5 +96,147 @@ class _InsxPageState extends State<InsxPage> {
           );
   }
 
-  Widget showListInsx() => Text('มีข้อมูล');
+  Color calculageHues(String notidate) {
+    List<Color> colors = [Colors.green, Colors.yellow, Colors.blue, Colors.red];
+    List<String> strings = notidate.split(" ");
+    List<String> dateTimeInts = strings[0].split('-');
+    DateTime notiDateTime = DateTime(
+      int.parse(dateTimeInts[0]),
+      int.parse(dateTimeInts[1]),
+      int.parse(dateTimeInts[2]),
+    );
+
+    DateTime currentDateTime = DateTime.now();
+    int diferDate = currentDateTime.difference(notiDateTime).inDays;
+    Color result = colors[0];
+
+    if (diferDate >= 7) {
+      result = colors[3];
+    } else if (diferDate >= 4) {
+      result = colors[2];
+    } else if (diferDate >= 2) {
+      result = colors[1];
+    }
+    return result;
+  }
+
+  Future<Null> confirmDialog(
+      InsxModel2 insxModel2, Color color, int index) async {
+    showDialog(
+      context: context,
+      builder: (context) => SimpleDialog(
+        title: ListTile(
+          leading: Icon(
+            Icons.pin_drop,
+            size: 36,
+            color: color,
+          ),
+          title: Text(insxModel2.cus_name),
+          subtitle: Text(insxModel2.pea_no),
+        ),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('กรุณาเลือก Camera'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                  icon: Icon(Icons.camera),
+                  onPressed: () {
+                    chooseCamera(index);
+                    Navigator.pop(context);
+                  }),
+              IconButton(
+                  icon: Icon(Icons.save),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget showListInsx() => ListView.builder(
+        itemCount: insxModel2s.length,
+        shrinkWrap: true,
+        physics: ScrollPhysics(),
+        itemBuilder: (context, index) => GestureDetector(
+          onTap: () =>
+              confirmDialog(insxModel2s[index], colorIcons[index], index),
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.pin_drop,
+                        size: 36,
+                        color: colorIcons[index],
+                      ),
+                      title: Text(insxModel2s[index].cus_name),
+                      subtitle: Text(
+                        '${insxModel2s[index].write_id} \n${insxModel2s[index].pea_no}',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: files[index] == null
+                        ? Text('No Image')
+                        : Container(
+                            height: 90,
+                            child: Image(
+                              fit: BoxFit.cover,
+                              image: FileImage(files[index]),
+                            ),
+                          ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+
+  Future<Null> chooseCamera(int index) async {
+    try {
+      var object = await ImagePicker().getImage(
+        source: ImageSource.camera,
+        maxWidth: 800,
+        maxHeight: 800,
+      );
+      setState(() {
+        files[index] = File(object.path);
+        uploadImage(files[index], index);
+      });
+    } catch (e) {}
+  }
+
+  Future<Null> uploadImage(File file, int index) async {
+    String apiSaveFile = 'https://pea23.com/apipsinsx/saveFile.php';
+    
+    TimeOfDay timeOfDay = TimeOfDay.now();
+    String fileName = 'image${Random().nextInt(1000000)}${insxModel2s[index].ca}.jpg';
+
+    try {
+      Map<String, dynamic> map = Map();
+      map['file'] = await MultipartFile.fromFile(file.path, filename: fileName);
+      FormData data = FormData.fromMap(map);
+      await Dio().post(apiSaveFile, data: data).then((value) {
+        print('====>>> $value');
+        print(
+          '===== Success url Image ==>> https://pea23.com/apipsinsx/upload/$fileName');
+      });
+    } catch (e) {}
+  }
 }
--- lib/pages/map_insx.dart
+++ lib/pages/map_insx.dart
@@ -1,38 +1,81 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:psinsx/models/insx_model2.dart';
 
 class MapInsx extends StatefulWidget {
+  final List<InsxModel2> insxModel2s;
+  MapInsx({@required this.insxModel2s});
+
   @override
   _MapInsxState createState() => _MapInsxState();
 }
 
 class _MapInsxState extends State<MapInsx> {
+  List<InsxModel2> insxModel2s;
+
+  LatLng startMapLatLng = LatLng(16.753188, 101.203616);
+
   @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          showMap(),
-        ],
-      ),
-    );
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    insxModel2s = widget.insxModel2s;
   }
 
-  Container showMap() {
-    LatLng latLng = LatLng(16.753209, 101.203873);
-    CameraPosition cameraPosition = CameraPosition(
-      target: latLng,
-      zoom: 16.0,
+  Set<Marker> myAllMarker() {
+    List<Marker> markers = List();
+    List<double> hues = [80.0, 60.0, 150.0, 20.0];
+    for (var item in insxModel2s) {
+      Marker marker = Marker(
+        icon: BitmapDescriptor.defaultMarkerWithHue(
+            calculageHues(item.noti_date)),
+        markerId: MarkerId('id${item.id}'),
+        position: LatLng(double.parse(item.lat), double.parse(item.lng)),
+        infoWindow: InfoWindow(title: item.cus_name, snippet: item.pea_no),
+      );
+      markers.add(marker);
+    }
+    return markers.toSet();
+  }
+
+  double calculageHues(String notidate) {
+    List<double> hues = [80.0, 60.0, 150.0, 20.0];
+    List<String> strings = notidate.split(" ");
+    List<String> dateTimeInts = strings[0].split('-');
+    DateTime notiDateTime = DateTime(
+      int.parse(dateTimeInts[0]),
+      int.parse(dateTimeInts[1]),
+      int.parse(dateTimeInts[2]),
     );
 
-    return Container(
-      height: 300,
-      child: GoogleMap(
-        initialCameraPosition: cameraPosition,
+    DateTime currentDateTime = DateTime.now();
+    int diferDate = currentDateTime.difference(notiDateTime).inDays;
+    double result = hues[0];
+
+    if (diferDate >= 7) {
+      result = hues[3];
+    } else if (diferDate >= 4) {
+      result = hues[2];
+    } else if (diferDate >= 2) {
+      result = hues[1];
+    }
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: startMapLatLng,
+          zoom: 6,
+        ),
         mapType: MapType.normal,
-        onMapCreated: (controller) {},
+        onMapCreated: (controller) => {},
+        markers: myAllMarker(),
+        myLocationButtonEnabled: true,
+        myLocationEnabled: true,
       ),
     );
   }
